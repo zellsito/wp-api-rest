faf6e21d7cbdfff4013e1f5e0db0bb63134e2c44
Luciano Salgado <lucho@luxton.com.ar>
2022-07-22 20:57:02 -0300

    asd

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Express Base
+# WhatsApp HTTP API REST
 
-This is a template de express. Use it with Docker or Docker Compose!
+This is a WhatsApp HTTP API REST based
+It uses:
+- pedroslopez whatsapp-web.js library (puppeteer, chromium)
+- NodeJS Express
+- qr-image to render QR code to HTML
 
 
 ## Installation
@@ -18,8 +22,8 @@ npm run dev
 ## Installation with docker
 
 ```bash
-docker build -t express-base .
-docker run --name express-base -d -p 3000:3000 express-base
+docker build -t wp-http .
+docker run --name wp-http -d -p 3000:3000 wp-http
 ```
 
 or
@@ -31,7 +35,15 @@ docker-compose up -d
 
 ## Usage
 
-GET:/ 
+GET:/ Get QR
+
+POST:/
+
+{
+  "password": "123456",
+  "number": "5491169940853",
+  "text": "Hola Mundo"
+}
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 const { Client, LocalAuth  } = require('whatsapp-web.js');
 const client = new Client({
   authStrategy: new LocalAuth(),
 });
 
-const qr = require('qr-image');
+const qrImage = require('qr-image');
 
 app.listen(port, () => {
   console.log(`WhatsApp API REST listening at http://localhost:${port}`);
 });
 
 app.post('/', (req, res) => {
-  client.sendMessage("5491169940853@c.us", "hola");
+  if (req.body.password != '123456') {
+    res.status(403).send();  
+  }
+  client.sendMessage(`${req.body.number}@c.us`, req.body.text);
+  res.send();
 })
 
-
-client.on('qr', (qrtxt) => {
+client.on('qr', (qr) => {
   app.get('/', (req, res) => {
-    let svg_string = qr.imageSync(qrtxt, { type: 'svg' });
+    let svg_string = qrImage.imageSync(qr, { type: 'svg' });
     res.send(svg_string);
   })
   console.log('QR RECEIVED', qr);
@@ -28,7 +34,6 @@ client.on('qr', (qrtxt) => {
 
 client.on('ready', () => {
   console.log('Client is ready!');
-  client.sendMessage("5491169940853@c.us", "hola");
 });
 
 client.on('message', msg => {
