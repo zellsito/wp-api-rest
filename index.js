05afd9c3af2245a304e280d05ff19e69f98e63fe
Luciano Salgado <lucho@luxton.com.ar>
2022-07-22 21:12:07 -0300

    asd

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 const { Client, LocalAuth  } = require('whatsapp-web.js');
-const client = new Client({
-  authStrategy: new LocalAuth(),
-});
+const client = new Client(/*{
+  
+}*/
+{ authStrategy: new LocalAuth(), puppeteer: { headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']}});
 
 const qrImage = require('qr-image');
 
